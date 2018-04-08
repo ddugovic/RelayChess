9d682c2bc7ebbfe2cce4b9799268f2191e0d788c
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-08 04:37:35 +0200

    private config shouldn't be commited

--- config.js
+++ config.js
@@ -3,19 +3,9 @@ module.exports = {
     apiServerPort: 9090,
     socketServerPort: 3000,
     oauth: {
-      client: {
-        // change these to your Lichess oauth app,
-        // that you can create on https://lichess.org/account/oauth/app
-        id: 'q9xRRHB5uuZt3Fui',
-        secret: 'JcC0B4TyTA6mQbI6TVixSDYYjF5wrz1A',
-        // change this to your local host, or the prod host
-        redirectUri: 'http://localhost:9090/login-with-lichess/callback',
-      },
-      server: {
-        tokenHost: 'https://oauth.lichess.org',
-        authorizePath: '/oauth/authorize',
-        tokenPath: '/oauth'
-      },
+      tokenHost: 'https://oauth.lichess.org',
+      authorizePath: '/oauth/authorize',
+      tokenPath: '/oauth',
       scopes: [
         // probably won't ever need any scope
       ]
---
+++ userToken.js
@@ -0,0 +1,32 @@
+function createUserToken(user, signature)
+{
+    //create token from the hash name and expire date
+    //note this isn't the secure (Twitch) token but simply a convenience
+    var token = {
+        name: user.name,
+        displayName: user.displayName,
+        expire: Date.now(),
+        signature: signature // twitch token
+    };
+    return token;
+}
+
+function validateUserToken(token)
+{
+    try{
+        var url = "https://api.twitch.tv/kraken?oauth_token=" + token.signature;
+        $.getJSON(url, function(data) {
+            return (token.name == data.token.user_name);
+        });
+        return false;
+    }
+    catch(ex)
+    {
+        return false;
+    }
+}
+
+module.exports = {
+    createUserToken: createUserToken,
+    validateUserToken: validateUserToken
+}

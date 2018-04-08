44d23b5c2df22ec8ac45bf48ec7d31750c89b931
Daniel Dugovic <dandydand@gmail.com>
2018-04-08 16:20:11 -0500

    Remove more twitch crap

--- userToken.js
+++ userToken.js
@@ -1,23 +1,10 @@
-function createUserToken(user, signature)
-{
-    //create token from the hash name and expire date
-    //note this isn't the secure (Twitch) token but simply a convenience
-    var token = {
-        name: user.name,
-        displayName: user.displayName,
-        expire: Date.now(),
-        signature: signature // twitch token
-    };
-    return token;
-}
-
 function validateUserToken(token)
 {
     try{
-        var url = "https://api.twitch.tv/kraken?oauth_token=" + token.signature;
-        $.getJSON(url, function(data) {
-            return (token.name == data.token.user_name);
-        });
+        //var url = "https://api.twitch.tv/kraken?oauth_token=" + token.signature;
+        //$.getJSON(url, function(data) {
+        //    return (token.name == data.token.user_name);
+        //});
         return false;
     }
     catch(ex)
@@ -27,6 +14,5 @@ function validateUserToken(token)
 }
 
 module.exports = {
-    createUserToken: createUserToken,
     validateUserToken: validateUserToken
 }

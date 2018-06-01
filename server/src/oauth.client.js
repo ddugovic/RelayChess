ebb17e504622863b473069906022c72caa2bed29
mnemi <22787082+mnemi@users.noreply.github.com>
2018-06-01 19:37:45 -0400

    Remove private config... again.

--- server/src/oauth.client.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // change these to your Lichess oauth app,
-  // that you can create on https://lichess.org/account/oauth/app
-  id: '',
-  secret: '',
-  // change this to your local host, or the prod host
-  redirectUri: 'http://localhost:9090/login-with-lichess/callback',
-};

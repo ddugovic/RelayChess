17fbe6854b1458ff3a136a6d49210486d3fb6d4d
Daniel Dugovic <dandydand@gmail.com>
2020-05-19 03:17:33 -0500

    Use named export CleanWebpackPlugin

--- web/webpack.common.js
+++ web/webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
@@ -15,7 +15,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             inject: 'head'
